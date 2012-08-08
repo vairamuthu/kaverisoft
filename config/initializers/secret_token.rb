afd71b0cacb73b2b721cd9c04facfa1613fb4a45
vairamuthu <vairamuthu.it@gmail.com>
2012-08-08 07:23:20 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaverisoft::Application.config.secret_token = 'e1b4a7c3937ef52300099cce80bbcb3e892c328efd860d5855647f604a730281c63a169617e2ff2fd142a74a04d78acbdaa6a8282b44be99d403704eee22e029'
